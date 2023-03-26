54837aec14b876394605a1a75292ec074d65c735
tanmaysihare <tanmaysihare@gmail.com>
2023-03-26 18:12:45 +0530

    adding fillter element or component which do nothing

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import ItemDisplay from './components/Expences/ItemDisplay';
 import Card from './components/UI/Card';
 
 const App=() => {
-  let [expences, setExpences] = useState([{date: new Date(2023, 1, 12), expanceName:'car', amount:560000, location:'delhi'},
-  {date: new Date(2023, 0, 22), expanceName:'QLED TV', amount:56000, location:'delhi'},
-  {date: new Date(2023, 3, 24), expanceName:'AC', amount:15000, location:'delhi'},
-  {date: new Date(2023, 6, 22), expanceName:'PS5', amount:55000, location:'delhi'}
+  let [expences, setExpences] = useState([{date: new Date(2022, 1, 12), expanceName:'car', amount:560000, location:'delhi'},
+  {date: new Date(2020, 0, 22), expanceName:'QLED TV', amount:56000, location:'delhi'},
+  {date: new Date(2021, 3, 24), expanceName:'AC', amount:15000, location:'delhi'},
+  {date: new Date(2022, 6, 22), expanceName:'PS5', amount:55000, location:'delhi'}
 ]);
   const addExpenceHandler = (expence) => {
     setExpences((expences)=>{
--- src/components/Expences/ItemDisplay.js
+++ src/components/Expences/ItemDisplay.js
@@ -1,13 +1,17 @@
 import './ItemDisplay.css';
-
+import React,{useState} from 'react';
+import ExpencesFilter from './ExpencesFilter';
 import ExpanceDetails from './ExpanceDetails';
 import Card from '../UI/Card';
 function ItemDisplay(props){
-  
+  const [filteredYear, setFilteredYear] = useState('2023');
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  };
     return(
         <Card>
            <div>
-          
+          <ExpencesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}>    </ExpencesFilter>
              {props.items.map((item) => (
           <ExpanceDetails
             key={item.id} // Make sure to assign a unique key to each rendered component
@@ -17,6 +21,7 @@ function ItemDisplay(props){
             amount={item.amount}
           />
         ))}
+    
            </div>
         </Card>
         
