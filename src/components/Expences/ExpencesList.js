1b249152a1467df3875995c3aecae117f9cb64b5
tanmaysihare <tanmaysihare@gmail.com>
2023-03-28 21:48:02 +0530

    right working of filter with new component

--- src/components/Expences/ExpencesList.js
+++ src/components/Expences/ExpencesList.js
@@ -2,36 +2,21 @@ import React from "react";
 import ExpanceDetails from "./ExpanceDetails";
 const ExpencesList = (props) => {
   
-    if(props.items.length === 1){
-      return ( 
-        <div>
-        {props.items.map((item) => (
-    <ExpanceDetails
-      key={item.id} 
-      date={item.date}
-      expanceName={item.expanceName}
-      location={item.location}
-      amount={item.amount}
-    />
-  ))
-  }
-  <h2>NO DATA FOUND ON A SELECTED DATE</h2>
-    </div>
-      )
-  
+    if(props.items.length === 0){
+      return  <h2>NO DATA FOUND ON A SELECTED DATE</h2>;
     }
     return(
         <div>
             {props.items.map((item) => (
-        <ExpanceDetails
-          key={item.id} 
-          date={item.date}
-          expanceName={item.expanceName}
-          location={item.location}
-          amount={item.amount}
-        />
-      ))
-      }
+                 <ExpanceDetails
+                     key={item.id} 
+                     date={item.date}
+                     expanceName={item.expanceName}
+                     location={item.location}
+                     amount={item.amount}
+                />
+            ))
+            }
         </div>
 
     );
