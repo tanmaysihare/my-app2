767ed32fcfc60e945bc597313689a73343e3d9a2
tanmaysihare <tanmaysihare@gmail.com>
2023-03-28 21:02:52 +0530

    making filter work

--- src/components/Expences/ItemDisplay.js
+++ src/components/Expences/ItemDisplay.js
@@ -5,22 +5,30 @@ import ExpanceDetails from './ExpanceDetails';
 import Card from '../UI/Card';
 function ItemDisplay(props){
   const [filteredYear, setFilteredYear] = useState('2023');
-  const filterChangeHandler = selectedYear => {
+  const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+  const filteredExpences = props.items.filter(expence =>{
+    return expence.date.getFullYear().toString() === filteredYear; 
+  });
+  let filterdContent = <p>NO DATA FOUND ON A SELECTED DATE</p>;
+  
+  if(filteredExpences.length > 0){
+    filterdContent = filteredExpences.map((item) => (
+      <ExpanceDetails
+        key={item.id} // Make sure to assign a unique key to each rendered component
+        date={item.date}
+        expanceName={item.expanceName}
+        location={item.location}
+        amount={item.amount}
+      />
+    ));
+  }
     return(
         <Card>
            <div>
-          <ExpencesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}>    </ExpencesFilter>
-             {props.items.map((item) => (
-          <ExpanceDetails
-            key={item.id} // Make sure to assign a unique key to each rendered component
-            date={item.date}
-            expanceName={item.expanceName}
-            location={item.location}
-            amount={item.amount}
-          />
-        ))}
+          <ExpencesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+          {filterdContent}
     
            </div>
         </Card>
