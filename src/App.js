eea5e9d5f3d474d8c48bbe58254be5f3f296a346
tanmaysihare <tanmaysihare@gmail.com>
2023-03-28 19:30:33 +0530

    making some styleing

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import React,{useState} from 'react';
 import './App.css';
 import NewExpence from './components/NewExpence';
 import ItemDisplay from './components/Expences/ItemDisplay';
-import Card from './components/UI/Card';
 
+
+const preExpences = [
+{id: 'e1', date: new Date(2022, 1, 12), expanceName:'car', amount:560000, location:'delhi'},
+{id: 'e2', date: new Date(2020, 0, 22), expanceName:'QLED TV', amount:56000, location:'delhi'},
+{id: 'e3', date: new Date(2021, 3, 24), expanceName:'AC', amount:15000, location:'delhi'},
+{id: 'e4', date: new Date(2022, 6, 22), expanceName:'PS5', amount:55000, location:'delhi'}
+];
 const App=() => {
-  let [expences, setExpences] = useState([{date: new Date(2022, 1, 12), expanceName:'car', amount:560000, location:'delhi'},
-  {date: new Date(2020, 0, 22), expanceName:'QLED TV', amount:56000, location:'delhi'},
-  {date: new Date(2021, 3, 24), expanceName:'AC', amount:15000, location:'delhi'},
-  {date: new Date(2022, 6, 22), expanceName:'PS5', amount:55000, location:'delhi'}
-]);
+  let [expences, setExpences] = useState(preExpences);
   const addExpenceHandler = (expence) => {
     setExpences((expences)=>{
-      return [...expences,expence];
+      return [expence,...expences];
     }); 
       
     
@@ -21,17 +23,14 @@ const App=() => {
   };
 
   return (
-    <Card>
-      <div className="MainBody2">
-      
+    <div className="app">
+      <div className="MainBody1">
       <NewExpence onAddExpence={addExpenceHandler} />
       </div>
       <div className="MainBody">
       <ItemDisplay items={expences}/>
-     
-     
       </div>
-    </Card>
+    </div>
   );
 }
 
--- src/components/ExpenceForm.js
+++ src/components/ExpenceForm.js
@@ -4,6 +4,7 @@ function ExpenceForm(props){
     const [enteredName, setEnteredName]= useState('');
     const [enteredAmount, setEnteredAmount]= useState('');
     const [enteredDate, setEnteredDate]= useState('');
+    const [enteredLocation, setEnteredLocation]= useState('');
 
     const ExpenceNameChangeHandler = (event) =>{
        setEnteredName(event.target.value);
@@ -14,32 +15,41 @@ function ExpenceForm(props){
     const DateChangeHandler = (event) => {
         setEnteredDate(event.target.value);
           };
+    const LocationChangeHandler = event =>{
+        setEnteredLocation(event.target.value);
+    };
     const SubmitHandler = (event) => {
         event.preventDefault();
         const ExpenceData = {
             expanceName: enteredName,
             amount: enteredAmount,
-            date: new Date(enteredDate)
+            date: new Date(enteredDate),
+            location: enteredLocation
         };
         props.onSaveExpanceData(ExpenceData);
         setEnteredName('');
         setEnteredAmount('');
         setEnteredDate('');
+        setEnteredLocation('');
     };
     return (
         <form onSubmit={SubmitHandler}>
             <div className="new-expense__controls">
-                <div className="new-expense__controls">
-                    <label className="col-form-label-lg">PLEASE INPUT EXPENCE NAME</label>
-                    <input type="text" className="form-control" value={enteredName} onChange={ExpenceNameChangeHandler} />
+                <div className="new-expense__control">
+                    <label className='label' >PLEASE INPUT EXPENCE NAME</label>
+                    <input type="text" className='input' value={enteredName} onChange={ExpenceNameChangeHandler} />
                 </div>
-                <div className="new-expense__controls">
-                    <label className="col-form-label-lg">PLEASE INPUT EXPENCE AMOUNT</label>
-                    <input type="number" min='0.01' step='0.01' className="form-control" value={enteredAmount} onChange={AmountChangeHandler}/>
+                <div className="new-expense__control">
+                    <label className="label">PLEASE INPUT EXPENCE AMOUNT</label>
+                    <input type="number" min='0.01' step='0.01' className="input" value={enteredAmount} onChange={AmountChangeHandler}/>
                 </div>
-                <div className="new-expense__controls">
-                    <label className="col-form-label-lg">PLEASE INPUT DATE</label>
-                    <input type="date" min="2019-01-01" max="2023-12-31" className="form-control" value={enteredDate} onChange={DateChangeHandler}/>
+                <div className="new-expense__control">
+                    <label className="label">PLEASE ADD LOCATION OF PECHASE</label>
+                    <input type="text" className="input" value={enteredLocation} onChange={LocationChangeHandler}/>
+                </div>
+                <div className="new-expense__control">
+                    <label className="label">PLEASE INPUT DATE</label>
+                    <input type="date" min="2019-01-01" max="2023-12-31" className="input" value={enteredDate} onChange={DateChangeHandler}/>
                 </div>
             </div>
             <div className="new-expense__actions">
